231a223e0ccad0873f39d67501ac2c80f2dbe94c
skordesign <skordesign@outlook.com>
2017-10-18 14:32:32 -0700

    add
    add cs

---
+++ AlmeButton.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Alme.Controls
+{
+    public class AlmeButton:Button
+    {
+        public static readonly BindableProperty StartColorProperty = BindableProperty.Create(
+        nameof(StartColor),
+        typeof(Color),
+        typeof(AlmeButton),
+        Color.Accent);
+        public Color StartColor
+        {
+            get { return (Color)GetValue(StartColorProperty); }
+            set { SetValue(StartColorProperty, value); }
+        }
+        public static readonly BindableProperty EndColorProperty = BindableProperty.Create(
+        nameof(EndColor),
+        typeof(Color),
+        typeof(AlmeButton),
+        Color.Accent);
+        public Color EndColor
+        {
+            get { return (Color)GetValue(EndColorProperty); }
+            set { SetValue(EndColorProperty, value); }
+        }
+        public static readonly BindableProperty CenterColorProperty = BindableProperty.Create(
+        nameof(CenterColor),
+        typeof(Color),
+        typeof(AlmeButton),
+        Color.Transparent);
+        public Color CenterColor
+        {
+            get { return (Color)GetValue(CenterColorProperty); }
+            set { SetValue(CenterColorProperty, value); }
+        }
+
+        public static readonly BindableProperty AngleProperty = BindableProperty.Create(
+        nameof(Angle),
+        typeof(int),
+        typeof(AlmeButton),
+        0);
+        public int Angle
+        {
+            get { return (int)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+        public AlmeButton()
+        {
+            this.Clicked += this.AlmeButton_Clicked;
+        }
+
+        private void AlmeButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Animation animateForClick = new Animation
+                {
+                    { 0, 1, new Animation(f => this.Scale = f, 1, 0.96, Easing.SinIn, null) }
+                };
+                Animation animateForClicked = new Animation
+                {
+                    { 0, 1, new Animation(f => this.Scale = f, 0.96, 1.02, Easing.SinOut, null) }
+                };
+                Animation animateForClickedReturn = new Animation
+                {
+                    { 0, 1, new Animation(f => this.Scale = f, 1.02, 1, Easing.SinIn, null) }
+                };
+                animateForClick.Commit(this, "BtnClick", 100, 100, 
+                    finished: (x, y) => animateForClicked.Commit(this, "BtnClicked", 100, 100,
+                    finished: (k,z)=> animateForClickedReturn.Commit(this,"Return",100,40)));
+
+            }
+            catch { }
+        }
+    }
+}
---
+++ AlmeButtonRenderer.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Alme.Droid.Renderers;
+using Alme.Controls;
+using Xamarin.Forms;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+
+[assembly: ExportRenderer(typeof(AlmeButton), typeof(AlmeButtonRenderer))]
+namespace Alme.Droid.Renderers
+{
+    public class AlmeButtonRenderer:ButtonRenderer
+    {
+        private AlmeButton almeButton;
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                try
+                {
+                    almeButton = e.NewElement as AlmeButton;
+                    GradientDrawable gradientDrawable = new GradientDrawable();
+                    gradientDrawable.SetShape(ShapeType.Rectangle);
+                    gradientDrawable.SetCornerRadius(almeButton.BorderRadius * 2);
+                    if (almeButton.CenterColor != Xamarin.Forms.Color.Transparent)
+                    {
+                        gradientDrawable.SetColors(new int[] { almeButton.EndColor.ToAndroid(),
+                almeButton.CenterColor.ToAndroid(), almeButton.StartColor.ToAndroid()});
+                    }
+                    else
+                    {
+                        gradientDrawable.SetColors(new int[] { almeButton.EndColor.ToAndroid(),
+                        almeButton.StartColor.ToAndroid()});
+                    }
+                    gradientDrawable.SetGradientType(GradientType.LinearGradient);
+                    gradientDrawable.SetOrientation(GradientDrawable.Orientation.RightLeft);
+#pragma warning disable CS0618 // Type or member is obsolete
+                    Control.SetBackgroundDrawable(gradientDrawable);
+#pragma warning restore CS0618 // Type or member is obsolete
+                    Control.SetTextColor(almeButton.TextColor.ToAndroid());
+                    if (almeButton.FontFamily != null)
+                    {
+                        Control.Typeface = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, almeButton.FontFamily.Split('#')[0]);
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
